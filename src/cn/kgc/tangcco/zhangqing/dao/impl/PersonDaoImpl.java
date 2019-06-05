b8f2ad44640e47f5352fd5eedbdafdc1f91430c5
xx <xx@DESKTOP-SJL4BO8>
2019-06-05 16:06:29 +0800

    第一次练习

---
+++ src/cn/kgc/tangcco/zhangqing/dao/impl/PersonDaoImpl.java
@@ -0,0 +1,16 @@
+package cn.kgc.tangcco.zhangqing.dao.impl;
+
+import cn.kgc.tangcco.zhangqing.dao.PersonDao;
+
+public class PersonDaoImpl implements PersonDao{
+
+
+	@Override
+	public void heihei() {
+	System.out.println("嘿嘿");
+		
+	}
+	
+
+
+}
